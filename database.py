0b0160f84c9f2e8116c511f851f09e8cbc139f0f
Pranav Phatak <pranavphatak99@gmail.com>
2026-09-06 15:19:00 +0530

    Tables created

--- database.py
+++ database.py
@@ -2,4 +2,34 @@ from langchain_community.utilities import SQLDatabase
 
 db = SQLDatabase.from_uri("sqlite:///personal_finance.db")
 
-print("Database created successfully")
+db.run("""
+    CREATE TABLE IF NOT EXISTS Expenses(
+        Id INTEGER PRIMARY KEY AUTOINCREMENT,
+        Category TEXT NOT NULL,
+        Amount REAL NOT NULL,
+        Description TEXT,
+        Expense_Date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
+""")
+
+db.run("""
+    CREATE TABLE IF NOT EXISTS Budgets(
+        Id INTEGER PRIMARY KEY AUTOINCREMENT,
+        Category TEXT NOT NULL,
+        Amount REAL NOT NULL CHECK(Amount > 0),
+        Month INTEGER NOT NULL CHECK(Month BETWEEN 1 AND 12),
+        Year INTEGER NOT NULL CHECK(Year BETWEEN 2000 AND 2100),
+        UNIQUE (Category, Month, Year)
+    );
+""")
+
+db.run("""
+    CREATE TABLE IF NOT EXISTS Income(
+        Id INTEGER PRIMARY KEY AUTOINCREMENT,
+        Amount INTEGER NOT NULL,
+        Source TEXT NOT NULL,
+        Income_Date DATE NOT NULL 
+    );
+""")
+
+print("Tables created successfully")
