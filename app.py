e0cd344e934371fb0e8664f83a0e071f2aedc773
Pranav Phatak <pranavphatak99@gmail.com>
2026-09-06 17:49:02 +0530

    completed the project

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import time
 import streamlit as st
 from agent import get_agent
 
@@ -20,32 +21,34 @@ if query:
 
     with st.chat_message("ai"):
         placeholder = st.empty()
+        status = st.empty()
         full_response = ""
-        with st.spinner("Processing your query..."):
-            for chunk in agent.stream(
-                {
-                    "messages": [
-                        {
-                            "role": "user",
-                            "content": query
-                        }
-                    ]
-                },
-                {
-                    "configurable": {
-                        "thread_id": "finance_agent"
+        status.markdown("🔄 Processing your query...")
+        response_started = False
+        for chunk in agent.stream(
+            {
+                "messages": [
+                    {
+                        "role": "user",
+                        "content": query
                     }
+                ]
+            },
+            {
+                "configurable": {
+                    "thread_id": "finance_agent"
                 }
-            ):
-                if "model" in chunk:
-                    messages = chunk["model"].get("messages", [])
-
-                    if messages:
-
-                        content = messages[-1].content
-
-                        if content:
-                            full_response += content
-                            placeholder.markdown(full_response)
+            }, stream_mode="messages"
+        ):
+            message_chunk, metadata = chunk
+            if message_chunk.type == "AIMessageChunk" and not message_chunk.tool_calls:
+                if message_chunk.content:
+                    if not response_started:
+                        status.empty()
+                        response_started = True
+                    content = message_chunk.content
+                    full_response += content
+                    placeholder.markdown(full_response)
+                    time.sleep(0.03)
             st.session_state.chat_history.append({"role":"ai", "content":full_response})
         
