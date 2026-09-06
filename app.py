8b0c06147920cb68c46257feea9436cd5a6bdd24
Pranav Phatak <pranavphatak99@gmail.com>
2026-09-06 17:30:14 +0530

    streaming done

--- app.py
+++ app.py
@@ -19,16 +19,33 @@ if query:
     st.chat_message("user").markdown(query)
 
     with st.chat_message("ai"):
+        placeholder = st.empty()
+        full_response = ""
         with st.spinner("Processing your query..."):
-            response = agent.invoke({
-                "messages":[
-                    {
-                        "role":"user",
-                        "content": query
+            for chunk in agent.stream(
+                {
+                    "messages": [
+                        {
+                            "role": "user",
+                            "content": query
+                        }
+                    ]
+                },
+                {
+                    "configurable": {
+                        "thread_id": "finance_agent"
                     }
-                ]
-            },{"configurable":{"thread_id":"finance_agent"}}
-            )
-            message = response['messages'][-1].content
-            st.session_state.chat_history.append({"role":"ai", "content":message})
-            st.markdown(message)
+                }
+            ):
+                if "model" in chunk:
+                    messages = chunk["model"].get("messages", [])
+
+                    if messages:
+
+                        content = messages[-1].content
+
+                        if content:
+                            full_response += content
+                            placeholder.markdown(full_response)
+            st.session_state.chat_history.append({"role":"ai", "content":full_response})
+        
